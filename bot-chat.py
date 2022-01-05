4d352dab2ec22596e17691ea6e82ce9e86e448d4
soybean15 <75112014+soybean15@users.noreply.github.com>
2022-01-05 20:39:55 +0800

    Update bot-chat.py

--- bot-chat.py
+++ bot-chat.py
@@ -1,18 +1,23 @@
 import re
 import random
+from math import ceil
 
 
-def reply_percentage(message, message_requirement):
+def reply_percentage(message, message_requirement, important):
     count = 0
     for i in message:
         if i in message_requirement:
             count += 1
+        if i in important:
+            count = len(message)
+
 
     percentage = 0
     if count != 0:
         percentage = float(count)/float(len(message))
 
-    return int(percentage)*100
+
+    return percentage*100
 
 
 def get_max(arr):
@@ -31,23 +36,26 @@ def generate_response(input_message):
     bot.append(['tapos na ko', 'busog pa'])#3
     bot.append(['nagcocode', 'wala pahinga ', 'eto nagfefacebook'])#4
     bot.append(['1 day old'])#5
+    bot.append(['seen','ok ok'])  # 5
 
     unknown = ['Sorry, I\'m a bot, I dont understad', 'ano besh?', 'di ko gets, pakilinaw']
     bot_int = []
 
-    bot_int.append(reply_percentage(input_message, ['hi', 'hello']))
-    bot_int.append(reply_percentage(input_message, ['kumusta', 'balita', 'ka', 'na']))
-    bot_int.append(reply_percentage(input_message, ['kumain','kain','ka', 'na']))
-    bot_int.append(reply_percentage(input_message, ['gawa', 'ginagawa']))
-    bot_int.append(reply_percentage(input_message, ['ilan', 'taon']))
+    bot_int.append(reply_percentage(input_message, ['hi', 'hello'],['']))
+    bot_int.append(reply_percentage(input_message, ['kumusta', 'balita'],['kumusta','balita']))
+    bot_int.append(reply_percentage(input_message, ['kumain','kain'],['']))
+    bot_int.append(reply_percentage(input_message, ['gawa', 'ginagawa'],['']))
+    bot_int.append(reply_percentage(input_message, ['ilan', 'taon'],['']))
+    bot_int.append(reply_percentage(input_message, ['okay', 'k', 'ok'],['']))
 
     max = get_max(bot_int)
+
 
     if max == 0:
         reply=unknown[random.randint(0, len(unknown)-1)]
     else:
         index = bot_int.index(max)
-        print(max)
+
         rand = random.randint(0, len(bot[index])-1)
         reply = bot[index][rand]
 
@@ -56,6 +64,7 @@ def generate_response(input_message):
 
 def get_response(input_message):
     split_message = re.split(r"[-;,.\s]\s*", input_message)
+
     return generate_response(split_message)
 
 
