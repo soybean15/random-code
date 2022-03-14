af9f6617517f517661a5f16b0401ce6518a866d0
soybean15 <75112014+soybean15@users.noreply.github.com>
2022-03-14 17:56:07 +0800

    Create TicTactoe.java

---
+++ TicTactoe.java
@@ -0,0 +1,221 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class Player{
+	int number;
+	char piece;
+	public Player(int number, char piece){
+		this.number=number;
+		this.piece= piece;
+	}
+
+    
+	int generateMove(int[] arr){
+		Random rand = new Random();
+		
+		int index = rand.nextInt(arr.length);
+		return arr[index];
+	}
+   
+	
+	
+}
+
+
+class Board{
+	char[][] board = new char[3][9];
+	int[] availableMove = {1,2,3,4,5,6,7,8,9};
+	
+	
+	public Board(){
+		createBoard();
+		
+	}
+	
+	void updateBoard(int row, int col, char piece){
+		board[row][col] = piece;
+	}
+	
+	void createBoard(){
+		char[] box ={'[',' ',']'};
+		int index = 0;
+		for(int i =0; i<3; i++){
+			index=0;
+			
+			for(int j =0; j<9; j++){
+					
+				if(j==3 ||j ==6 ){
+					index=0;
+				}
+				board[i][j] = box[index];
+				index++;				
+				
+			}
+		}
+	}
+
+	
+	
+	
+	void printBoard(){
+		for(int i =0; i<3; i++){
+			for(int j =0; j<9; j++){
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	
+	int checkElement(int n){
+		int count = 0;
+		for(int i : availableMove){
+			if (n==i){
+				return count;
+			}
+			count++;
+		}
+		return -1;
+	}
+	
+	int[] updateAvailableMove(int n){
+		int target = checkElement(n);
+		int[] newArr= new int[availableMove.length-1];
+		if(target <0){
+			return availableMove;
+		}
+		else{
+			
+			for(int i = 0; i <target; i++){
+				newArr[i] =availableMove[i];
+			}
+			
+			for(int i=target; i<newArr.length;i++){
+				newArr[i] = availableMove[i+1];
+			}
+		}
+		
+
+		return newArr;
+	}
+}
+
+public class TicTactoe{
+	int[][] winningMove ={{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9},{3,5,7}};
+
+	int[] row = {0,0,0,1,1,1,2,2,2};
+	int[] col = {1,4,7,1,4,7,1,4,7};
+	Scanner sc = new Scanner(System.in);
+	Board board = new Board();
+    boolean draw = true;
+    Player[] player = new Player[2];
+
+    
+	
+	boolean checkWinner(List<Integer> arr){	
+		for(int i=0; i<8; i++){
+			int count =0;
+			for(int j = 0; j<3; j++){
+				for(int k :arr){
+					if(winningMove[i][j] ==k){
+						count++;
+					}
+				}
+				if (count==3){
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	
+	}
+	boolean checkMove(int[] arr, int n){
+		for(int i :arr){
+			if(i==n){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	void start(){
+        
+		player[0] =  new Player(1,'x');
+		player[1] =  new Player(2,'o');
+		List<Integer> p1move = new ArrayList<>();
+        List<Integer> p2move = new ArrayList<>();
+    
+            
+            int index =0;
+            board.printBoard();
+            while(board.availableMove.length>0){
+            System.out.println("index:"+index);
+			System.out.println("Player "+(player[index].number));
+			
+			System.out.println("Select move:");
+            int move = 0;
+            if(player[index].number == 2){
+                move = player[index].generateMove(board.availableMove);
+                p1move.add(move);
+            }else{
+                do{
+                    move = sc.nextInt();
+                    System.out.println("Already Picked");
+                     System.out.println("Select Move:");
+                }while(!checkMove(board.availableMove, move));
+                    
+                
+                p2move.add(move);            
+            }
+			
+			
+				
+				board.updateBoard(row[move-1], col[move-1], player[index].piece);
+				board.printBoard();
+				board.availableMove = board.updateAvailableMove(move);
+
+				
+				
+
+				if(checkWinner(p2move) || checkWinner(p1move) ){
+					System.out.println("Winner: Player"+player[index].number);
+                    draw= false;
+					break;
+				}
+				
+		
+            index++;
+            if(index == 2){
+                index=0;
+            }
+			
+			
+		}
+        if(draw){
+            System.out.println("Draw");
+        }
+      
+        
+        
+		
+			
+		
+	}
+	public static void main(String[] args){
+		
+	TicTactoe tictactoe = new TicTactoe();
+	
+	 tictactoe.start();
+	//Bot bot = new Bot(2,'0');
+		
+	//System.out.println("Original Array: "+Arrays.toString(bot.availableMove));
+	//bot.availableMove = bot.updateAvailableMove(2);
+	//System.out.println("Updated Array: "+Arrays.toString(bot.availableMove));
+	}
+}
